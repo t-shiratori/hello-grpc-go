e3254b36f10431a0c90931f773359649826a7fb8
Takashi Shiratori <tks.srtr89@gmail.com>
2022-09-04 17:14:52 +0900

    add files

---
+++ client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	go_protocol_buffer "hello-grpc-go/go-protocol-buffer"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := go_protocol_buffer.NewPinPonServiceClient(conn)
+
+	callPinPon(client)
+}
+
+func callPinPon(client go_protocol_buffer.PinPonServiceClient) {
+
+	reqWords := ""
+
+	if len(os.Args) >= 2 {
+		reqWords = os.Args[1]
+	}
+
+	println("reqWords:", reqWords)
+
+	res, err := client.Send(context.Background(), &go_protocol_buffer.PinPonRequest{Words: reqWords})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetWords())
+}
---
+++ server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	go_protocol_buffer "hello-grpc-go/go-protocol-buffer"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	go_protocol_buffer.UnimplementedPinPonServiceServer
+}
+
+func (s *server) Send(ctx context.Context, req *go_protocol_buffer.PinPonRequest) (*go_protocol_buffer.PinPonResponse, error) {
+
+	println("req.Words:", req.Words)
+
+	resWords := ""
+
+	if req.Words == "Pin" {
+		resWords = "Pon!"
+	} else {
+		resWords = "Please need words 'Pin'!"
+	}
+
+	res := &go_protocol_buffer.PinPonResponse{
+		Words: resWords,
+	}
+
+	return res, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	go_protocol_buffer.RegisterPinPonServiceServer(grpcServer, &server{})
+
+	fmt.Println("server is runnig...")
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
